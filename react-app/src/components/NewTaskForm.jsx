5e97db27ee4a23330722a1e21fb1e503cb28304e
Alicia Buelow <alicia@buelow.com.au>
2026-03-24 16:15:21 +0800

    Set up file structure and React with Vite

---
+++ react-app/src/components/NewTaskForm.jsx
@@ -0,0 +1,7 @@
+export default function NewTaskForm() {
+    return (
+        <div>
+            NewTaskForm
+        </div>
+    )
+}
---
+++ react-app/src/components/ProjectList.jsx
@@ -0,0 +1,7 @@
+export default function ProjectList() {
+    return (
+        <div>
+            Project List
+        </div>
+    )
+}
